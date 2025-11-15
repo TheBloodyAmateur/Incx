6b3b4c5c171eba19b1152e9aa72ad33d78f92185
TheBloodyAmateur <botan_kadir@outlook.com>
2025-11-15 14:30:52 +0100

    inital commit

---
+++ backend/src/main/java/com/github/thebloodyamateur/incx/IncxApplication.java
@@ -0,0 +1,13 @@
+package com.github.thebloodyamateur.incx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncxApplication.class, args);
+	}
+
+}
