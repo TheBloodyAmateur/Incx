8882885195e19543adc16c1dc00dc56fe8826966
TheBloodyAmateur <botan_kadir@outlook.com>
2025-11-26 18:20:23 +0100

    fix routing issue for filestorage page

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/login/LoginPage";
 import DashboardPage from "../pages/dashboard/DashboardPage";
 import WeatherPage from "../pages/weather/WeatherPage";
+import FileStoragePage from "../pages/filestorage/FileStoragePage";
+import BookingPage from "../pages/booking/BookingPage";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +18,10 @@ const router = createBrowserRouter([
     {
         path: "/weather",
         element: <WeatherPage />,
+    },
+    {
+        path: "/filestorage",
+        element: <FileStoragePage />,
     }
 ]);
 
